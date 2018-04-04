531521f8c46b7958ac2838e6f813a5d41089837f
Intelivex Labs <puja@intelivexlabs.com>
2018-04-04 12:10:07 +0530

    Latest imageEditor changes with pod ZMJimageEditor

---
+++ CustomImageFilter/CustomImageFilter/ViewControllers/CustomImageFilter-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  CustomImageFilter-Bridging-Header.h
+//  CustomImageFilter
+//
+//  Created by Intelivex Labs on 03/04/18.
+//  Copyright © 2018 Intelivex Labs. All rights reserved.
+//
+
+#ifndef CustomImageFilter_Bridging_Header_h
+#define CustomImageFilter_Bridging_Header_h
+
+
+#endif /* CustomImageFilter_Bridging_Header_h */
